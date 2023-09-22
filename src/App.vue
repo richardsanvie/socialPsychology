e17a0ef9a6bfc7640c41cc6a02cfbe7459b1886b
Richard <richardsanvie@gmail.com>
2023-09-22 15:11:35 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <script setup>
+import { reactive } from 'vue';
 
+
+const estado = reactive({
+  tarefas:
+    [
+      {
+        titulo: 'Estudar ES6',
+        finalizada: false,
+      },
+      {
+        titulo: 'Estudar Sass',
+        finalizada: false,
+      },
+      {
+        titulo: 'Ir a academia',
+        finalizada: true,
+      }
+
+    ]
+})
+
+function validaValor() {
+  return
+}
 </script>
 
 <template>
@@ -9,31 +33,43 @@
     </div>
     <div class="container p-1">
       <div class="container-fluid">
-  <div class="row">
-    <div class="col-5">
-      <label for="tarefa" class="sr-only">Tarefa:</label>
-      <input type="text" id="tarefa" class="form-control" placeholder="Tarefa">
-    </div>
-    <div class="col-3">
-      <button type="submit" class="btn btn-primary mt-4">Adicionar</button>
-    </div>
-    <div class="col-4">
-      <label for="filtro" class="sr-only">Filtrar:</label>
-      <select id="filtro" class="form-control">
-        <option value="tds">Todas tarefas</option>
-        <option value="Pendentes">Pendentes</option>
-        <option value="finalizadas">Finalizadas</option>
-      </select>
-    </div>
-  </div>
-</div>
+        <div class="row">
+          <div class="col-5">
+            <label for="tarefa" class="sr-only">Tarefa:</label>
+            <input type="text" id="tarefa" class="form-control" placeholder="Tarefa">
+          </div>
+          <div class="col-3">
+            <button type="submit" class="btn btn-primary mt-4">Adicionar</button>
+          </div>
+          <div class="col-4">
+            <label for="filtro" class="sr-only">Filtrar:</label>
+            <select id="filtro" class="form-control">
+              <option value="tds">Todas tarefas</option>
+              <option value="Pendentes">Pendentes</option>
+              <option value="finalizadas">Finalizadas</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <ul class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in estado.tarefas">
+          <input type="checkbox" :checked="tarefa.finalizada" :id="tarefa.titulo" @keyup="checkado => checkado.target.click">
+          <label class="ms-3" :for="tarefa.titulo">
+            {{ tarefa.titulo }}
+          </label>
+        </li>
+      </ul>
 
-      <p class="m-2">lorem</p>
+      <!-- <p class="m-2">lorem</p>
       <hr class="m-0">
       <p class="m-2">lorem</p>
-      <hr class="m-0">
+      <hr class="m-0"> -->
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
